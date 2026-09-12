b27ddff0f454194322359c9bbe0ea55a051e1743
malleshwari <malleshwaribandapalli@gmail.com>
2026-09-12 11:19:00 +0530

    Add files via upload

---
+++ float.py
@@ -0,0 +1,10 @@
+Telugu = 87
+Hindi = 89
+English = 97
+Maths = 98
+Science = 94
+Social = 75
+total = Telugu + Hindi + English + Maths + Science + Social
+average = total/6
+print(average)
+print(type(average))
