e74e6b6238912978292f7c3f8b5ffe7fe159d431
malleshwari <malleshwaribandapalli@gmail.com>
2026-09-12 11:15:52 +0530

    Add files via upload

---
+++ comparison.py
@@ -0,0 +1,8 @@
+a = 10
+b = 4
+print("equal:" , a== b)
+print("not equal:",a!=b)
+print("greater than:",a>b)
+print("less than:", a<b)
+print("greater then or equal:",a<=b)
+print("less than or equal:",a>=b)
