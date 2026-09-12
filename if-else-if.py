e0e362f30aeb6c2718b2ab4eedef7997bf5d9f80
malleshwari <malleshwaribandapalli@gmail.com>
2026-09-12 11:21:02 +0530

    Add files via upload

---
+++ if-else-if.py
@@ -0,0 +1,13 @@
+marks = 75
+if marks>=90:
+ print("Excellent")
+elif marks>=80:
+ print("good")
+elif marks>=75:
+ print("better")
+elif marks>=50:
+ print("average")
+elif marks>=40:
+ print("bad")
+else:
+ print("fail")
