6d025cd47318001fd580530fd4acf9214060a897
malleshwari <malleshwaribandapalli@gmail.com>
2026-09-12 11:22:31 +0530

    Add files via upload

---
+++ logical.py
@@ -0,0 +1,14 @@
+marks = 79
+age = 20
+is_student = True
+is_eligible = False
+print(marks>=75 and age >=19)
+print(marks>=80 and age>=19)
+print(marks>=75 and age>=22)
+print(marks>=80 and age>=22)
+print(marks>=75 or age >=19)
+print(marks>=80 or age>=19)
+print(marks>=75 or age>=22)
+print(marks>=80 or age>=22)
+print(not is_student)
+print(not is_eligible)
