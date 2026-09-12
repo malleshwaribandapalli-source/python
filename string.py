dc850cabb50a8a32317b138113e1ddb3bd30d272
malleshwari <malleshwaribandapalli@gmail.com>
2026-09-12 11:24:39 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,16 @@
+Name = "Malleshwari"
+Branch = "CSE"
+College = "Aditya college of engineering"
+Address = "Valasapalli"
+print(Name)
+print(Branch)
+print(College)
+print(Address)
+print(Name[0])
+print(Branch[-2])
+print(len(College))
+print(Address.upper())
+print(Name.lower())
+print(College.title())
+print(College.find(College))
+print("college" in  College)
