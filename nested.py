321435805c60378f11ed6374a3e916028fe61901
malleshwari <malleshwaribandapalli@gmail.com>
2026-09-12 11:23:19 +0530

    Add files via upload

---
+++ nested.py
@@ -0,0 +1,13 @@
+enter_password = True
+password_correct = True
+
+if enter_password:
+    print("Password accepted")
+
+    if password_correct:
+        print("Password correct")
+    else:
+        print("Wrong password")
+
+else:
+    print("Invalid password")
