ce660472c9026d6ea0e71a96a7f1ab6ca02148b9
malleshwari <malleshwaribandapalli@gmail.com>
2026-09-12 11:15:37 +0530

    Add files via upload

---
+++ bool.py
@@ -0,0 +1,13 @@
+name = "malleshwari"
+age = 20
+marks = 97
+is_student = True
+has_id = True
+print(name)
+print(age)
+print(marks)
+if marks>=18:
+ print(True)
+else:
+ print(false)
+print(type(is_student))
